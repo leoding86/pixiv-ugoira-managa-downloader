3456cb137cde51d07fea0bd93d8cdec6a526184b
Leo Ding <leoding86@gmail.com>
2018-07-10 22:32:48 +0800

    update save function

--- pages/src/components/UgoiraExtendDialog.vue
+++ pages/src/components/UgoiraExtendDialog.vue
@@ -12,6 +12,7 @@
                     <v-list-tile-action>
                         <v-select :items="secondsItems"
                                   v-model="enableWhenUnderSeconds"
+                                  @change="onEnableWhenUnderSecondsChangeHandler()"
                                   style="max-width: 100px"></v-select>
                     </v-list-tile-action>
                 </v-list-tile>
@@ -24,6 +25,7 @@
                     <v-list-tile-action>
                         <v-select :items="extendDurationItems"
                                   v-model="extendDuration"
+                                  @change="onExtendDurationChangeHandler()"
                                   style="max-width: 100px"></v-select>
                     </v-list-tile-action>
                 </v-list-tile>
@@ -33,6 +35,8 @@
 </template>
 
 <script>
+import cr from '@/modules/cr'
+
 export default {
   name: 'UgoiraExtendDialog',
   data () {
@@ -48,11 +52,39 @@ export default {
       extendDuration: 10
     }
   },
+  mounted () {
+      if (window.cr.storage.items.enableWhenUnderSeconds) {
+          this.enableWhenUnderSeconds = window.cr.storage.items.enableWhenUnderSeconds
+      }
+
+      if (window.cr.storage.items.extendDuration) {
+          this.extendDuration = window.cr.storage.items.extendDuration
+      }
+  },
   watch:  {
       showDialog: function (val) {
           if (!!val === false) {
-              this.$router.go(-1);
+              this.$router.go(-1)
           }
+      }
+  },
+  methods: {
+      onEnableWhenUnderSecondsChangeHandler (evt) {
+          var _this = this
+          cr._s.set({
+              enableWhenUnderSeconds: _this.enableWhenUnderSeconds
+          }).then(() => {
+              window.cr.storage.items.enableWhenUnderSeconds = _this.enableWhenUnderSeconds
+          })
+      },
+
+      onExtendDurationChangeHandler (evt) {
+          var _this = this
+          cr._s.set({
+              extendDuration: _this.extendDuration
+          }).then(() => {
+              window.cr.storage.items.extendDuration = _this.extendDuration
+          })
       }
   }
 }
