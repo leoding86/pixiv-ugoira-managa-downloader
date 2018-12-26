9141dbc8025aa7a29fd47f7275ea940be0a30ce8
leoding86 <leoding86@msn.com>
2018-12-26 10:46:41 +0800

    add settings about downloading manga

--- pages/src/components/Options.vue
+++ pages/src/components/Options.vue
@@ -37,6 +37,42 @@
                 </v-list-tile>
             </v-list>
         </v-card>
+        <span class="card-title">Manga</span>
+        <v-card>
+            <v-list two-line>
+                <v-list-tile @click="showSaveMangaTitleDialog()">
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{ _i('saved_manga_file_title') }}</v-list-tile-title>
+                        <v-list-tile-sub-title>{{ _i('saved_manga_file_title_desc') }}</v-list-tile-sub-title>
+                    </v-list-tile-content>
+                    <v-list-tile-action>
+                        <v-btn icon ripple>
+                            <v-icon>keyboard_arrow_right</v-icon>
+                        </v-btn>
+                    </v-list-tile-action>
+                </v-list-tile>
+                <v-list-tile @click="showSaveMangaImagesTitleDialog()">
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{ _i('saved_manga_image_file_title') }}</v-list-tile-title>
+                        <v-list-tile-sub-title>{{ _i('saved_manga_image_file_title_desc') }}</v-list-tile-sub-title>
+                    </v-list-tile-content>
+                    <v-list-tile-action>
+                        <v-btn icon ripple>
+                            <v-icon>keyboard_arrow_right</v-icon>
+                        </v-btn>
+                    </v-list-tile-action>
+                </v-list-tile>
+                <v-list-tile>
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{ _i('manga_image_name_prefix') }}</v-list-tile-title>
+                        <v-list-tile-sub-title>{{ _i('manga_image_full_name_intro') }}</v-list-tile-sub-title>
+                    </v-list-tile-content>
+                    <v-list-tile-action>
+                        <v-text-field v-model="mangaImageNamePrefix"></v-text-field>
+                    </v-list-tile-action>
+                </v-list-tile>
+            </v-list>
+        </v-card>
         <router-view />
     </v-container>
 </template>
@@ -60,11 +96,23 @@ export default {
                     value: 1
                 }
             ],
-            ugoiraQuanlity: 10
+            ugoiraQuanlity: 10,
+
+            mangaImageNamePrefix: '',
         }
     },
     mounted () {
         this.ugoiraQuanlity = window.cr.storage.items.ugoiraQuanlity ? window.cr.storage.items.ugoiraQuanlity : this.ugoiraQuanlity - 0;
+        this.mangaImageNamePrefix = window.cr.storage.items.mangaImageNamePrefix ? window.cr.storage.items.mangaImageNamePrefix : '';
+    },
+
+    watch: {
+        mangaImageNamePrefix (value) {
+            console.log(value);
+            cr._s.set({
+                'mangaImageNamePrefix': value
+            });
+        }
     },
 
     methods: {
@@ -85,6 +133,14 @@ export default {
             cr._s.set({ 'ugoiraQuanlity': _this.ugoiraQuanlity } )
         },
 
+        showSaveMangaTitleDialog: function() {
+            this.$router.push('save-manga-title');
+        },
+
+        showSaveMangaImagesTitleDialog: function() {
+            this.$router.push('save-manga-images-title');
+        },
+
         _i (string) {
             return cr._e(string);
         }
@@ -94,6 +150,10 @@ export default {
 
 <style lang="scss" scoped>
 #app {
+    .v-card {
+        margin-bottom: 30px;
+    }
+
     .card-title {
         display: block;
         font-size: 18px;
