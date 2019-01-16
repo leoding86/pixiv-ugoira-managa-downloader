6932b355b5d5d2c758e922e3db6f20465bac4835
leoding86 <leoding86@gmail.com>
2019-01-16 11:17:23 +0800

    update router

--- pages/src/router/index.js
+++ pages/src/router/index.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Options from '@/components/Options'
-import UgoriaSaveTitleDialog from '@/components/UgoiraSaveTitleDialog'
 import UgoriaExtendDialog from '@/components/UgoiraExtendDialog'
-import SaveMangaTitleDialog from '@/components/SaveMangaTitleDialog'
-import SaveMangaImagesTitleDialog from '@/components/SaveMangaImagesTitleDialog'
 import RenameUgoiraDialog from '@/components/RenameUgoiraDialog';
 import RenameMangaDialog from '@/components/RenameMangaDialog';
 import RenameMangaImageDialog from '@/components/RenameMangaImageDialog';
@@ -19,21 +16,9 @@ export default new Router({
       component: Options,
       children: [
           {
-              path: 'ugoira-save-title',
-              name: 'UgoiraSaveTitle',
-              component: UgoriaSaveTitleDialog,
-          }, {
               path: 'ugoira-extend',
               name: 'UgoiraExtend',
               component: UgoriaExtendDialog,
-          }, {
-              path: 'save-manga-title',
-              name: 'SaveMangaTitle',
-              component: SaveMangaTitleDialog,
-          }, {
-              path: 'save-manga-images-title',
-              name: 'SaveMangaImagesTitle',
-              component: SaveMangaImagesTitleDialog,
           }, {
               path: 'rename-ugoira',
               name: 'RenameUgoira',
